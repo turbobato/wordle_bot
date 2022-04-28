cd9a6aa1448ce3adc9dbfa00debd0fc06aac81e1
Guilhem Jazeron <guilhem.jazeron@telecom-paris.fr>
2022-04-28 19:39:41 +0200

    v1 du bot fonctionnel mais il compte les carrés jaune en double

--- bot.py
+++ bot.py
@@ -1,38 +1,47 @@
 #!/usr/bin/env python3
 # Import the os module.
-import asyncio
 import os
-
+# Import the random module for randomly choosing words
+import random
+# Import asyncio for dealing with async funcs
+import asyncio
+# Import csv module for generating the wordlist
+import csv
 # Import load_dotenv function from dotenv module.
 from dotenv import load_dotenv
 
+import string
+
 # Loads the .env file that resides on the same level as the script.
 load_dotenv()
-# Grab the API token from the .env file.
+# Grab the tokens from the .env file.
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+#CHANNEL= int(os.getenv("CHANNEL_ID"))
+# Imports for the bot
 import discord
 from discord.ext import tasks, commands
+# Create a bot
+client = commands.Bot(command_prefix='!')
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# Create the wordlist
 
-client = discord.Client()
+with open("wordlist.csv", "r") as csvlist :
+    wordlist=[]
+    csvreader=csv.reader(csvlist)
+    for word in csvreader :
+        wordlist+= word
 
-f = open("cascada.txt", "r")
+nb_words=len(wordlist)
 
-phrase_list = ["Points", "Samuel <3", "h. <3<3", "road to top1", "#PandouEnSueur"]
+letters = string.ascii_uppercase
+letters
 
-def file_to_list(file) :
-    return [x for x in file]
+black_square=":black_large_square:"
+green_square=":green_square:"
+yellow_square=":yellow_square:"
+skull=":skull:"
 
-#phrase_list = file_to_list(f)
-
-async def my_background_task():
-    await client.wait_until_ready()
-    counter = -1
-    channel = client.get_channel(id=897981369882398774) # replace with channel_id
-    while not client.is_closed():
-        counter = (counter + 1) % len(phrase_list)
-        await channel.send(phrase_list[counter])
-        await asyncio.sleep(60) # task runs every 60 seconds
+games_ongoing = {} 
 
 @client.event
 async def on_ready():
@@ -41,6 +50,50 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-client.loop.create_task(my_background_task())
+@client.command()
+async def start(ctx):
+    user = ctx.author
+    if not user in games_ongoing :
+        games_ongoing[user]=[0, wordlist[random.randrange(0,nb_words)], set(letters)]
+    elif games_ongoing[user][0] in range(0,6) :
+        await ctx.send(f'{user}, you\'re already playing a game !')
+        return
+    else :
+        games_ongoing[user]=[0, wordlist[random.randrange(0,nb_words)], set(letters)]
+    await ctx.send("You can now play a game by inputing five letter words preceded by !guess")
 
-client.run(os.getenv("DISCORD_TOKEN"), bot = False)
+@client.command()
+async def guess(ctx, word_guess):
+    user=ctx.author
+    word_guess=word_guess.upper()
+    if games_ongoing[user][0] not in range(0,6):
+        await ctx.send(f"{user}, you must start a game before starting to play ! Please run !start")
+        return
+    if not len(word_guess)==5:
+        await ctx.send(f'{user}, the word must be of length 5')
+    elif word_guess not in wordlist :
+        await ctx.send(f'{user}, this word doesn\'t exist')
+    else :
+        pattern=""
+        for i in range(5):
+            letter=word_guess[i]
+            correct_letter=games_ongoing[user][1][i]
+            games_ongoing[user][2]= games_ongoing[user][2]-set(letter)
+            if letter==correct_letter :
+                pattern+=green_square
+            elif letter in games_ongoing[user][1]:
+                pattern+=yellow_square
+            else:
+                pattern+=black_square
+        games_ongoing[user][0]+=1
+        if games_ongoing[user][0]==6:
+            if word_guess==games_ongoing[user][1]:
+                await ctx.send(f"Congrats {user}, you win !")
+            else : 
+                await ctx.send(f"{user}, you lost...{skull}\nThe word was {games_ongoing[user][1]}")
+        else :        
+            await ctx.send(str(user)+":\n"+pattern+"\n Number of tries "+str(games_ongoing[user][0])+" /6"+"\n" \
+                    +"Here are the letters you haven't used yet : "+' ,'.join(sorted(list(games_ongoing[user][2]))))
+
+            
+client.run(os.getenv("DISCORD_TOKEN"))
